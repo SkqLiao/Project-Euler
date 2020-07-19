44924c181f1b4dd4d53ad399f723e52573ad9ca7
Skqliao <skqliao@gmail.com>
2020-07-19 20:55:35 +0800

    update T24

--- code/24.cpp
+++ code/24.cpp
@@ -3,9 +3,8 @@
 int F[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 int main() {
-	for (int i = 1; i < int(1E6); ++i) {
+	for (int i = 1; i < int(1E6); ++i)
 		std::next_permutation(F, F + 10);
-	}
 	for (int i = 0; i < 10; ++i) 
 		printf("%d", F[i]);
 	return 0;
