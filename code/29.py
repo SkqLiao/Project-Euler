cbe0288f9eba8330a7a1dd5c29183b59cecefeba
Skqliao <skqliao@gmail.com>
2020-07-21 20:42:23 +0800

    update T29

--- code/29.py
+++ code/29.py
@@ -12,5 +12,5 @@ def F(a, b):
 	return len(set(i ** j for i in range(2, a + 1) for j in range(2, b + 1)))
 
 if __name__ == '__main__':
-	print(solve(100, 100))
+	#print(solve(100, 100))
 	print(F(100, 100))
