bd018fe8727b15677e23b0aee9c8a8ab18fd2d1a
Skqliao <skqliao@gmail.com>
2020-07-21 20:44:31 +0800

    update T30

--- code/30.py
+++ code/30.py
@@ -1,10 +1,6 @@
-
 if __name__ == '__main__':
 	total = 0
 	for i in range(10, 6 * 9 ** 5 + 1):
-		s = str(i)
-		cur = 0
-		for j in s:
-			cur += int(j) ** 5
+		cur = sum(int(j) ** 5 for j in str(i))
 		if cur == i: total = total + i
 	print(total)
