165b4ea56e493f65c3f3a342885a9d39408fc448
SkqLiao <skqliao@gmail.com>
2020-08-16 14:40:00 +0800

    update

--- code/51-100/52.py
+++ code/51-100/52.py
@@ -1,18 +1,19 @@
-
 def check(x):
-	f = [x * i for i in range(1, 7)]
-	ori = list(str(x))
-	ori.sort()
-	for i in range(0, 6):
-		cur = list(str(f[i]))
-		cur.sort()
-		if ori != cur: return False
-	return True
+    f = [x * i for i in range(1, 7)]
+    ori = list(str(x))
+    ori.sort()
+    for i in range(0, 6):
+        cur = list(str(f[i]))
+        cur.sort()
+        if ori != cur:
+            return False
+    return True
+
 
 if __name__ == '__main__':
-	i = 0
-	while True:
-		i += 1
-		if check(i):
-			print(i)
-			exit(0)
+    i = 0
+    while True:
+        i += 1
+        if check(i):
+            print(i)
+            break
